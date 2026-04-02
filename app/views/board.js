8e57e0c2fe1eac4354be62d9f3c46ab0366d55ba
DevVinayShriffle <vinayks@shriffle.com>
2026-04-02 12:15:21 +0530

    Added event

--- app/views/board.js
+++ app/views/board.js
@@ -3,7 +3,15 @@ const _ = require('underscore')
 
 const BoardView=Marionette.View.extend({
   tagName: 'div',
-  template: _.template(require('../templates/board.html').default)
+  template: _.template(require('../templates/board.html').default),
+
+  events: {
+    'click .mybutton': 'alertBox',
+  },
+
+  alertBox: function() {
+    alert("You have clicked on mybtn click")
+  }
 })
 
 module.exports=BoardView;
